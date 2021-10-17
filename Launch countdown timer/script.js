96d5b3814a2e35f808f2ff225770f42bb83925fd
Paradizzo <80402261+paradizzo@users.noreply.github.com>
2021-10-17 17:24:13 -0300

    Update script.js

--- Launch countdown timer/script.js
+++ Launch countdown timer/script.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const endDate = new Date('May 30, 2021 00:00:00').getTime()
+    const endDate = new Date('May 30, 2022 00:00:00').getTime()
     const now = new Date().getTime()
     const difference = endDate - now
     const seconds = 1000
